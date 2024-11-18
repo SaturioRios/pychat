113bab0704e956e8007df5f7f8a1e46fb7b67116
SaturioRios <proyectogrupo5saturio@gmail.com>
2024-11-18 09:28:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'  # Cambia 'your_secret_key' por una clave secure
@@ -17,4 +18,5 @@ def handleMessage(msg):
     send(msg, broadcast=True)  # Envía el mensaje a todos los clients conectados
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render asignará el puerto
+    socketio.run(app, host='0.0.0.0', port=port)
